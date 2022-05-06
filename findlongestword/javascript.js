a52aa84f717da74536b3eb41ab7c79451a0f248f
ziloka <50429450+ziloka@users.noreply.github.com>
2022-05-06 09:41:04 -0400

    Update javascript.js

--- findlongestword/javascript.js
+++ findlongestword/javascript.js
@@ -2,14 +2,9 @@ const value1 = process.argv[2];
 
 // write your solution here
 let arr = value1.split(',').map((str) => str.trim().toLowerCase());
-let result = [];
-let longest = '';
+let result = [''];
 for (let word of arr) {
-    if (longest.length < word.length) {
-        longest = word;
-        result = [longest];
-    } else if(longest.length == word.length) {
-        result.push(word);
-    }
+    if (result[0].length < word.length) result = [word];
+    else if(result[0].length === word.length) result.push(word);
 }
 console.log(result.join(','));
