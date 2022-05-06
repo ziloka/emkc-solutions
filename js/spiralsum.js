232b22ba68de01cf62107b30db0c9029e520c723
ziloka <50429450+ziloka@users.noreply.github.com>
2022-05-06 08:32:15 -0400

    Create spiralsum.js

---
+++ js/spiralsum.js
@@ -0,0 +1,10 @@
+// optimized
+const value1 = parseInt(process.argv[2]);
+
+// write your solution here
+let difference = value1-1;
+let bottomLeft = value1**2;
+let bottomRight = bottomLeft-difference;
+let topRight = bottomLeft-(difference*2);
+let topLeft = bottomLeft-(difference*3);
+console.log(bottomLeft+bottomRight+topRight+topLeft);
