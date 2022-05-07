ac1d7f1a0580f5b0939e8f94ada3aa3acf71bbdf
ziloka <50429450+ziloka@users.noreply.github.com>
2022-05-06 23:40:00 -0400

    Update original.js

--- zippermerge/original.js
+++ zippermerge/original.js
@@ -8,13 +8,8 @@ let result = [];
 let switchArr = true;
 let currentIndex = 0;
 for (let i = 0; i < (arr1.length+arr2.length); i++) {
-    if (switchArr) {
-        result.push(arr1[currentIndex]);
-        switchArr = false;
-    } else {
-        result.push(arr2[currentIndex]);
-        switchArr = true;
-    }
+    result.push((switchArr ? arr1 : arr2)[currentIndex]);
+    switchArr = switchArr ? false : true;
     if (switchArr === true) currentIndex++;
 }
 
