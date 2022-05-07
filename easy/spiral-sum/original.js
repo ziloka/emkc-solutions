86d788cf623b7658329c73c5a310e89cfaad71a5
ziloka <50429450+ziloka@users.noreply.github.com>
2022-05-07 10:18:39 -0400

    Update original.js

--- easy/spiral-sum/original.js
+++ easy/spiral-sum/original.js
@@ -4,34 +4,34 @@ const value1 = process.argv[2];
 // https://code.likeagirl.io/create-a-nxn-matrix-spiral-with-javascript-955ee18402f7
 function matrix(n) {
     let result = new Array(n).fill().map(() => new Array(n).fill('')); // create empty n x n array
-    let counter = 1;
+    let counter = n * n;
     let startCol = 0;
     let endCol = n - 1;
     let startRow = 0;
     let endRow = n - 1;
     while (startCol <= endCol && startRow <= endRow) {
         for (let i = startCol; i <= endCol; i++) {
-            result[startRow][i] = (n*n) - counter + 1;
-            counter++;
+            result[startRow][i] = counter;
+            counter--;
         }
         startRow++;
         for (let j = startRow; j <= endRow; j++) {
-            result[j][endCol] = (n*n) - counter + 1;
-            counter++;
+            result[j][endCol] = counter;
+            counter--;
         }
         endCol--;
         for (let i = endCol; i >= startCol; i--) {
-            result[endRow][i] = (n*n) - counter + 1;
-            counter++;
+            result[endRow][i] = counter;
+            counter--;
         }
         endRow--;
         for (let i = endRow; i >= startRow; i--) {
-            result[i][startCol] = (n*n) - counter + 1;
-            counter++;
+            result[i][startCol] = counter;
+            counter--;
         }
         startCol++;
     }
-    return result.map((a) => a.reverse());
+    return result.reverse();
 }
 
 let arr = matrix(Number(value1.split('x')[0]));
