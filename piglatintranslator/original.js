0f0cdf80804f5704d642af878d74cca0b80ea22a
ziloka <50429450+ziloka@users.noreply.github.com>
2022-05-06 23:29:28 -0400

    Update original.js

--- piglatintranslator/original.js
+++ piglatintranslator/original.js
@@ -6,40 +6,19 @@ let vowels = ['a', 'e', 'i', 'o', 'u']
 let result = '';
 for (let word of words) {
     if (word.endsWith('yay')) {
-        word = word.slice(0, word.length - 3);
-        result = result + ' ' + word;
+        result = result + word.slice(0, word.length - 3) + ' ';
     } else {
         word = word.slice(0, word.length - 2);
         let consonantSequence = '';
         for (let i = word.length-1; i > 0; --i) {
-            if (!vowels.includes(word[i])) {
+            if (!vowels.includes(word[i]) && word[i] != '-') {
                 consonantSequence = word[i] + consonantSequence;
             } else {
                 break;
             }
         }
-        word = word.slice(word.length-2) + word.slice(0, word.length-2);
-        result = result + ' ' + word;
+        word = word.slice(word.length-consonantSequence.length) + word.slice(0, word.length-consonantSequence.length-1);
+        result = result + word + ' ';
     }
 }
-console.log(result.replace(/-/gmi, ''));
-
-// translate english to pig latin
-// let words = value1.split(' ');
-// let vowels = ['a', 'e', 'i', 'o', 'u'];
-// let result = '';
-// for (let word of words) {
-//     if (vowels.includes(word[0])) {
-//       result = result + ' ' + word + 'yay';
-//     } else {
-//       let consonantSequence = '';
-//       for (let letter of word) {
-//         if (!vowels.includes(word)) {
-//           consonantSequence = consonantSequence + letter;
-//         } else {
-//           break;
-//         }
-//       }
-//       result = result + ' ' + word.slice(consonantSequence.length) + consonantSequence + '-ay';
-//     }
-// }
+console.log(result);
