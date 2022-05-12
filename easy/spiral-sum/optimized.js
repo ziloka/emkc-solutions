46cb74061ea78a9f41749c328c80ceea7b3ad461
ziloka <50429450+ziloka@users.noreply.github.com>
2022-05-12 10:04:11 -0400

    Update optimized.js

--- easy/spiral-sum/optimized.js
+++ easy/spiral-sum/optimized.js
@@ -3,7 +3,7 @@ const value1 = parseInt(process.argv[2]);
 // write your solution here
 let difference = value1-1;
 let corner1 = value1**2;
-let corner2 = bottomLeft-difference;
-let corner3 = bottomLeft-(difference*2);
-let corner4 = bottomLeft-(difference*3);
+let corner2 = corner1-difference;
+let corner3 = corner1-(difference*2);
+let corner4 = corner1-(difference*3);
 console.log(corner1+corner2+corner3+corner4);
