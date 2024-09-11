857bf4cec5d7520d3927b6814a81b42d826fab9e
Ziloka <50429450+ziloka@users.noreply.github.com>
2024-09-10 20:09:26 -0400

    Update shortest.js

--- easy/spiral-sum/shortest.js
+++ easy/spiral-sum/shortest.js
@@ -3,4 +3,4 @@ const n = parseInt(process.argv[2]);
 // write your solution here
 // solution from the optimized solution but the algebraic expression is simplified
 console.log(n*n*4-(n-1)*6);
-// explanation: https://math.preview.excalidraw.com/#json=aSc1pCmPhKw1uXjfteNGV,mkgbhKNxO4QNz2Ws6mTQsw
+// https://math.preview.excalidraw.com/#json=Gj5Gfm8Z6GPkT4vykU70k,-EZBAy-UujRwCZzlFFUsqA
